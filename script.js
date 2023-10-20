c72ffe6ea66310bbf26fc550aab2715abb5d46b3
Draggie306 <75524491+Draggie306@users.noreply.github.com>
2023-10-20 17:20:49 +0000

    fix: wait for DOM content load for event listener initialisation

--- script.js
+++ script.js
@@ -1,42 +1,44 @@
-const form = document.querySelector('form');
-const output = document.querySelector('#output');
-
-form.addEventListener('submit', async (event) => {
-    event.preventDefault();
-
-    const dataText = form.elements['data_text'].value;
-    const width = form.elements['width'].value;
-    const height = form.elements['height'].value;
-    const maxFontSize = form.elements['max_font_size'].value;
-    output.innerHTML = "Loading response...";
-
-    try {
-        const response = await fetch('https://geog-nea-wordcloud.draggie.repl.co/generate_word_cloud', {
-            method: 'POST',
-            headers: {
-            'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ data_text: dataText, width: width, height: height, max_font_size: maxFontSize })
-        });
-
-        if (response.ok) {
-            const result = await response.json();
-            // get wordcloud base64 image from json response "wordcloud" field
-            wordcloud_result = result.wordcloud;
-            let base64Image = wordcloud_result.split(';base64,').pop();
-            // create image element with base64 image
-            let image = document.createElement('img');
-            image.src = `data:image/png;base64,${base64Image}`;
-            // set output to image
-            output.innerHTML = ` <a href="${wordcloud_result}" download="wordcloud.png">Download Word Cloud</a>`;
-        } else {
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('form');
+    const output = document.querySelector('#output');
+    
+    form.addEventListener('submit', async (event) => {
+        event.preventDefault();
+    
+        const dataText = form.elements['data_text'].value;
+        const width = form.elements['width'].value;
+        const height = form.elements['height'].value;
+        const maxFontSize = form.elements['max_font_size'].value;
+        output.innerHTML = "Loading response...";
+    
+        try {
+            const response = await fetch('https://geog-nea-wordcloud.draggie.repl.co/generate_word_cloud', {
+                method: 'POST',
+                headers: {
+                'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ data_text: dataText, width: width, height: height, max_font_size: maxFontSize })
+            });
+    
+            if (response.ok) {
+                const result = await response.json();
+                // get wordcloud base64 image from json response "wordcloud" field
+                wordcloud_result = result.wordcloud;
+                let base64Image = wordcloud_result.split(';base64,').pop();
+                // create image element with base64 image
+                let image = document.createElement('img');
+                image.src = `data:image/png;base64,${base64Image}`;
+                // set output to image
+                output.innerHTML = ` <a href="${wordcloud_result}" download="wordcloud.png">Download Word Cloud</a>`;
+            } else {
+                // get json "message" field if it exists, otherwise use the status text
+                const result = await response.json();
+                output.innerHTML = `Error generating word cloud, server says: ${result.message || response.statusText}`;
+            }
+        } catch (error) {
+            console.error('Error:', error);
             // get json "message" field if it exists, otherwise use the status text
-            const result = await response.json();
-            output.innerHTML = `Error generating word cloud, server says: ${result.message || response.statusText}`;
+            output.innerHTML = `Error generating word cloud! ${error.message || error}`;
         }
-    } catch (error) {
-        console.error('Error:', error);
-        // get json "message" field if it exists, otherwise use the status text
-        output.innerHTML = `Error generating word cloud! ${error.message || error}`;
-    }
+    });
 });
