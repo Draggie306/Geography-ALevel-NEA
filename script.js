106577aa98c9f19791c0487c8beb0702159f7221
Draggie306 <75524491+Draggie306@users.noreply.github.com>
2023-10-20 12:00:51 +0000

    add frontend

---
+++ script.js
@@ -0,0 +1,24 @@
+const form = document.querySelector('form');
+const output = document.querySelector('#output');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const dataText = form.elements['data_text'].value;
+  const width = form.elements['width'].value;
+  const height = form.elements['height'].value;
+  const maxFontSize = form.elements['max_font_size'].value;
+  output.innerHTML = "Loading responce...";
+    
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', '/generate_word_cloud');
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.onload = () => {
+    if (xhr.status === 200) {
+      output.innerHTML = `<img src="${xhr.response}" alt="Word Cloud">`;
+    } else {
+      output.innerHTML = 'Error generating word cloud';
+    }
+  };
+  xhr.send(JSON.stringify({ data_text: dataText, width: width, height: height, max_font_size: maxFontSize }));
+});
