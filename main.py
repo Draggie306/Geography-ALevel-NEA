c6a606e07ec1517aedab5ab457dca5531ec42f12
Draggie306 <75524491+Draggie306@users.noreply.github.com>
2023-10-20 17:01:12 +0000

    much improved server generation and handling

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import uuid
 import random
 import flask
 import traceback
+import base64
 from collections import Counter
 
 from flask_cors import CORS, cross_origin
@@ -41,34 +42,30 @@ def generate_word_cloud():
     try:
         width = 1920
         height = 1080
-    
+
         print(request.json)
-        words = request.json.get('data_text')
-        width = request.json.get('width')
-        height = request.json.get('height')
-        max_font_size = request.json.get('max_font_size')
-    
-        # Change depending on width/height
-        # For 1080p a good size is around 140
-        #max_font_size = 300#int(input("Enter max font size: (recommended: 30% of height, 300 for default)\n>>> "))
-    
-        data_text = ' '.join(words)
-        
-        words = re.findall(r'\b\w+\b', data_text.lower())  # Regex to find all words in the text data
-        amount_of_words = len(words)
-        print(f"\nThere are {amount_of_words} words in the text.")
-        
+        try:
+            width = int(request.json.get('width'))
+            height = int(request.json.get('height'))
+            max_font_size = int(request.json.get('max_font_size'))
+        except Exception:
+            return jsonify({
+                "error": True,
+                "message": "Invalid literal value type, please use numbers"
+            }), 403
+
+
+        data = request.json
+        words = re.findall(r'\b\w+\b', data.get('data_text', '').lower())
+
         word_counts = Counter(words)
-        print("Most common words:")
-        
+        print(f"\nThere are {len(words)} words in the text.")
+        print("\nMost common words:")
         for word, count in word_counts.most_common():
             print(f"{word}: {count}")
-        
-        print(f"\nAmount of words: {amount_of_words}")
-        
-        # now generate word cloud - https://www.datacamp.com/tutorial/wordcloud-python
-        text = data_text
-        
+
+        text = ' '.join(words)
+
         print(f"\nGenerating word cloud with resolution {width}x{height}. Max wordsize = {max_font_size}")
         wordcloud = WordCloud(
             max_font_size=max_font_size,  # more common words should be the biggest
@@ -83,17 +80,15 @@ def generate_word_cloud():
             prefer_horizontal=0.85,  # 80% chance of horizontal, which is easier to read
             #color_func=lambda *args, **kwargs: "hsl(330, 100%%, %d%%)" % random.randint(50, 80)
             ).generate(text)
-        
+
         plt.figure(dpi=200)
         plt.imshow(wordcloud, interpolation="bilinear")
         plt.axis("off")
-        
+
         # uuid_to_use = uuid.uuid4()
         # TODO: Change from uuid to time + DateTime
         uuid_to_use = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
 
-        
-        
         if not repl_save:
             print("saving to local directory as {}".format(uuid_to_use))
             wordcloud.to_file(f"D:\\OneDrive - Notre Dame High School\\[nea] Geo\\word cloud\\python_wordcloud_{uuid_to_use}.png")
@@ -101,10 +96,18 @@ def generate_word_cloud():
             print(f'Saving on filesystem in generations folder as "{uuid_to_use}"')
             wordcloud.to_file(f"generations/python_wordcloud_{uuid_to_use}.png")
 
+
+        base64wordcloud = base64.b64encode(open(f"generations/python_wordcloud_{uuid_to_use}.png", 'rb').read())
+        base64wordcloud = base64wordcloud.decode('utf-8')
+
         return jsonify({
-            "wordcloud": wordcloud
+            "error": False,
+            "message": "Successfully generated word cloud",
+            "wordcloud": base64wordcloud
         }), 200
     except Exception as e:
+        print(e)
+        print(traceback.format_exc())
         return jsonify({
                 "error": True,
                 "message": f"Error generating response: {e}\n\n{traceback.format_exc()}"
