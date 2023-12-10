51563778d3a1c6d64699e6fed4126973ff1bf91c
Draggie306 <75524491+Draggie306@users.noreply.github.com>
2023-12-10 22:29:25 +0000

    feat: coloured word generation

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
-import csv
 import re
-import time
 import datetime
-import uuid
 import random
-import flask
 import traceback
 import base64
+import colorsys
 from collections import Counter
 
 from flask_cors import CORS, cross_origin
 
-import matplotlib.pyplot as plt
 from wordcloud import WordCloud
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify
 
 data = []
 
@@ -24,8 +20,6 @@ repl_save = True
 # Default: 1920, 1080
 #width = int(input("Enter width: (recommended: 1920)\n>>> "))
 #height = int(input("Enter height: (recommended: 1080)\n>>> "))
-
-from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -54,11 +48,23 @@ def generate_word_cloud():
             width = int(request.json.get('width'))
             height = int(request.json.get('height'))
             max_font_size = int(request.json.get('max_font_size'))
+            rgb_red = int(request.json.get('r'))
+            rgb_green = int(request.json.get('g'))
+            rgb_blue = int(request.json.get('b'))
+            colour_variation = int(request.json.get('variation'))
         except Exception:
             return jsonify({
                 "error": True,
                 "message": "Invalid literal value type, please use numbers"
             }), 403
+
+        pixels = width * height
+
+        if pixels > 20000000:
+            return jsonify({
+                "error": True,
+                "message": "Width and height must be less, currently too big!"
+                }), 403
 
         data = request.json
         words = re.findall(r'\b\w+\b', data.get('data_text', '').lower())
@@ -70,6 +76,32 @@ def generate_word_cloud():
             print(f"{word}: {count}")
 
         text = ' '.join(words)
+
+        def color_func(r, g, b, variation):
+            # Convert RGB values to a scale of 0 to 1
+            r /= 255.0
+            g /= 255.0
+            b /= 255.0
+
+            # Convert RGB to HLS
+            h, l, s = colorsys.rgb_to_hls(r, g, b)
+
+            # Generate a random variation within the specified range
+            variation /= 200.0  # Convert percentage to decimal
+            hue_variation = random.uniform(-variation, variation)
+
+            # Adjust hue value with hue_variation
+            h = (h + hue_variation) % 1.0
+
+            # Convert HLS back to RGB
+            r, g, b = colorsys.hls_to_rgb(h, l, s)
+
+            # Convert RGB values back to a scale of 0 to 255
+            r = int(r * 255)
+            g = int(g * 255)
+            b = int(b * 255)
+
+            return "rgb({}, {}, {})".format(r, g, b)
 
         print(f"\nGenerating word cloud with resolution {width}x{height}. Max wordsize = {max_font_size}")
         wordcloud = WordCloud(
@@ -83,7 +115,7 @@ def generate_word_cloud():
             collocation_threshold=80,
             normalize_plurals=True,  # removes plurals - better for word cloud
             prefer_horizontal= 0.85,  # 80% chance of horizontal, which is easier to read
-            #color_func=lambda *args, **kwargs: "hsl(330, 100%%, %d%%)" % random.randint(50, 80)
+            color_func=lambda *args, **kwargs: color_func(rgb_red, rgb_green, rgb_blue, colour_variation)
         ).generate(text)
         """plt.figure(dpi=200)
         plt.imshow(wordcloud, interpolation="bilinear")
